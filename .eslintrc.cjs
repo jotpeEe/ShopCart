7c970aaf45a361e997149e7f43ac36a34d871588
mac <maciekklospl@gmail.com>
2023-02-21 15:12:28 +0100

    prettier and eslint additional setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,48 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+    overrides: [
+        {
+            extends: [
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            files: ['*.ts', '*.tsx'],
+            parserOptions: {
+                project: 'tsconfig.json',
+            },
+        },
     ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint', 'import'],
+    extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'external',
+                        position: 'before',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['react'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+    },
 };
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 80,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  arrowParens: "avoid",
 };
